157f9920b8f0b3c8feb64559049e6d99bdffe63c
MuhammadUmar7195 <mmuhammadumar869@gmail.com>
2025-08-08 20:36:06 +0500

    Add customer portal link in cors

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:2000"],
+    origin: [process.env.FRONTEND_URL, process.env.CUSTOMER_PORTAL_URL],
     credentials: true,
 }));
 
